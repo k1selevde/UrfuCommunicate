5b6d8eb69ccc4652e55b5718866b84fd8e30fbf4
Denis Kiselev <k1selevde@yandex.ru>
2020-05-01 12:40:41 +0500

    base markup in client

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import thunk from 'redux-thunk'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {Provider} from "react-redux";
+import {BrowserRouter as Router} from 'react-router-dom'
+import {createStore,compose,applyMiddleware} from 'redux'
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './redux/reducers/rootReducer'
+import App from './App';
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let store = createStore(rootReducer,
+    localStorage.getItem('store') ? JSON.parse(localStorage['store']) : {}
+    ,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+
+
+const app = (
+    <Provider store={store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ client/src/redux/reducers/sessionReducer.js
@@ -0,0 +1,14 @@
+let initialState = {
+    user: {
+        isAuth: false,
+        id: null,
+        token: null
+    },
+    errorMsg: '',
+    isLoading: false
+}
+
+export default (state = initialState, action) => {
+    return state;
+}
+
